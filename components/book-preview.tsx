824325d2379cc8694bec0dc937f1fbc51d9b193e
Show530 <sophiahowson@gmail.com>
2025-04-04 00:58:21 -0400

    adding author name

--- components/book-preview.tsx
+++ components/book-preview.tsx
@@ -2,6 +2,16 @@ import {BookEntry} from '@/new-types';
 // import {Rickroll} from "../public/Rickroll.jpg"
 
 export default function BookPreview({book}:{book:BookEntry}) {
+    // function author_name() {
+    //     if (book.book.cached_contributors[0].author.name != null) {
+    //         return <h3 className="font-bold">By {book.book.cached_contributors[0].author.name}</h3>
+    //     }
+    //     else {
+    //         return <h3></h3>
+    //     }
+    //
+    // }
+
     return(
         <div className="bg-sky-300 round-xl p-4 m-2 w-100 justify-center">
             <h2 className="font-bold text-3xl text-center">Finished: {book.last_read_date}</h2>
@@ -11,6 +21,7 @@ export default function BookPreview({book}:{book:BookEntry}) {
                      src={book.book.default_cover_edition?.cached_image?.url ?? "/Rickroll.jpg"}
                      alt={"Cover of " + book.book.title}/>
                 <h3 className="font-bold">{book.book.title}</h3>
+                <h3 className="font-bold">By {book.book.cached_contributors[0]?.author?.name ?? "Author Unknown"}</h3>
             </div>
         </div>
     );
